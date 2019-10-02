703fbccf9bd39e5b4d291c17d00a1fd70cb64439
tirso <tirso.cordova.briz@gmail.com>
2019-10-01 21:13:47 -0600

    completed basic CV

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 # FLASK
 from flask import Flask, jsonify, render_template, request
 import os, optparse
+import yaml
 
 app = Flask(__name__)
 
 developer = os.getenv("DEVELOPER", "Tirso")
-environment=os.getenv("ENVIRONMENT", "development")
+environment = os.getenv("ENVIRONMENT", "development")
+
+with open("info.yml", 'r', encoding='utf-8') as stream:
+    try:
+        info = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
+@app.route("/")
+def home():
+    foo = "bar"
+    return render_template("home.html", info=info)
+
+
+@app.route("/studies")
+def studies():
+    return render_template("studies.html", info=info['studies'])
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html", info=info['experience'])
 
 
 if __name__ == "__main__":
